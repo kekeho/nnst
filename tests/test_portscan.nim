149f55e7bac4e015a4e99e73b26c5555c59206d3
Hiroki.T <hirodora@me.com>
2019-09-01 14:14:07 +0900

    change: test server name

--- tests/test_portscan.nim
+++ tests/test_portscan.nim
@@ -18,7 +18,7 @@ suite "lib/portscan test":
     test "proc scan_port":
         const timeout = 2500
 
-        const valid_url = "test_server"
+        const valid_url = "test_server_portscan"
         const valid_port = 80
         const invalid_url = "dasfawoeitnyoaieawer.dafer2rfae"
         const invalid_port = 32013
@@ -30,7 +30,7 @@ suite "lib/portscan test":
     test "proc scan_port (list)":
         const timeout = 2500
 
-        const valid_url = "test_server"
+        const valid_url = "test_server_portscan"
         const valid_port_list = [80, 443]
         const invalid_port_list = [1, 2]
         const half_valid_port_list = [80, 9]  # [valid, invalid]
@@ -40,7 +40,7 @@ suite "lib/portscan test":
         assert scan_port(valid_url, half_valid_port_list, timeout) == [true, false]
     
     test "proc scan_port (range)":
-        const url = "test_server"
+        const url = "test_server_portscan"
         const valid_port_http = 80
         const valid_port_https = 443
         const invalid_port = 323
@@ -53,4 +53,4 @@ suite "lib/portscan test":
     
     # Shutdown test_server
     let sock = newSocket()
-    sock.connect("test_server", Port(9999))
+    sock.connect("test_server_portscan", Port(9999))
